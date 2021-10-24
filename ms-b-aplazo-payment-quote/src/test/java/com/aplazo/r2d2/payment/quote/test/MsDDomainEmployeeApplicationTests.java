262e22e3119816cf938b227f310a3ceb39382407
astorga645 <32880361+astorga645@users.noreply.github.com>
2021-10-23 22:55:36 -0600

    primer version
    primer version

---
+++ ms-b-aplazo-payment-quote/src/test/java/com/aplazo/r2d2/payment/quote/test/MsDDomainEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplazo.r2d2.payment.quote.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDDomainEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
